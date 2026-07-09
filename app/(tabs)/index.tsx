59bf0d562188be3d4bfe3ca44c27c7e144f3d2f5
Ngoc Qui <125120853+tnqui@users.noreply.github.com>
2026-07-09 10:32:19 +0700

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,23 @@
 import "@/global.css"
 import {Text, View} from "react-native";
 import {Link} from "expo-router";
+import {styled} from "nativewind";
+import {SafeAreaView as RNSafeAreaView} from 'react-native-safe-area-context';
 
+const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-background">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
+        <SafeAreaView className="flex-1 bg-background p-5">
+            <Text className="text-7xl font-vietnam-extrabold text-success">
+                Home
             </Text>
-            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+            <Link href="/onboarding" className="mt-4 font-vietnam-medium rounded bg-primary text-white p-4">
                 Go to boarding
             </Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">
+            <Link href="/(auth)/sign-in" className="mt-4 font-vietnam-medium  rounded bg-primary text-white p-4">
                 Go to login
             </Link>
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
+            <Link href="/(auth)/sign-up" className="mt-4 font-vietnam-medium rounded bg-primary text-white p-4">
                 Go to signup
             </Link>
 
@@ -30,7 +33,6 @@ export default function App() {
                 Claude Max subscriptions
             </Link>
 
-
-        </View>
+        </SafeAreaView>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,39 @@
-import { Stack } from "expo-router";
+import {SplashScreen, Stack} from "expo-router";
 import "@/global.css"
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown: false}}/>;
+    const [fontsLoaded] = useFonts({
+        'BeVietnamPro-Thin': require('../assets/fonts/BeVietnamPro-Thin.ttf'),
+        'BeVietnamPro-ThinItalic': require('../assets/fonts/BeVietnamPro-ThinItalic.ttf'),
+        'BeVietnamPro-ExtraLight': require('../assets/fonts/BeVietnamPro-ExtraLight.ttf'),
+        'BeVietnamPro-ExtraLightItalic': require('../assets/fonts/BeVietnamPro-ExtraLightItalic.ttf'),
+        'BeVietnamPro-Light': require('../assets/fonts/BeVietnamPro-Light.ttf'),
+        'BeVietnamPro-LightItalic': require('../assets/fonts/BeVietnamPro-LightItalic.ttf'),
+        'BeVietnamPro-Regular': require('../assets/fonts/BeVietnamPro-Regular.ttf'),
+        'BeVietnamPro-Italic': require('../assets/fonts/BeVietnamPro-Italic.ttf'),
+        'BeVietnamPro-Medium': require('../assets/fonts/BeVietnamPro-Medium.ttf'),
+        'BeVietnamPro-MediumItalic': require('../assets/fonts/BeVietnamPro-MediumItalic.ttf'),
+        'BeVietnamPro-SemiBold': require('../assets/fonts/BeVietnamPro-SemiBold.ttf'),
+        'BeVietnamPro-SemiBoldItalic': require('../assets/fonts/BeVietnamPro-SemiBoldItalic.ttf'),
+        'BeVietnamPro-Bold': require('../assets/fonts/BeVietnamPro-Bold.ttf'),
+        'BeVietnamPro-BoldItalic': require('../assets/fonts/BeVietnamPro-BoldItalic.ttf'),
+        'BeVietnamPro-ExtraBold': require('../assets/fonts/BeVietnamPro-ExtraBold.ttf'),
+        'BeVietnamPro-ExtraBoldItalic': require('../assets/fonts/BeVietnamPro-ExtraBoldItalic.ttf'),
+        'BeVietnamPro-Black': require('../assets/fonts/BeVietnamPro-Black.ttf'),
+        'BeVietnamPro-BlackItalic': require('../assets/fonts/BeVietnamPro-BlackItalic.ttf'),
+    });
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded])
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return <Stack screenOptions={{headerShown: false}}/>;
 }
